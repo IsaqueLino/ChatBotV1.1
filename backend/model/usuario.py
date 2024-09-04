bfbff7d983caba997eb13cf90b94669f95219d9b
Isaque Lino <102130217+IsaqueLino@users.noreply.github.com>
2024-09-04 16:22:45 -0300

    Update usuario.py

--- backend/model/usuario.py
+++ backend/model/usuario.py
@@ -1,32 +1,18 @@
-from controle import *
-
-
-class Participante:
+class Usuario:
 
     def __init__(self):
-        self.__idparticipante = ''
+        self.__idusuario = ''
         self.__nome = ''
-        self.__prontuario = ''
-        self.__cpf = ''
         self.__email = ''
         self.__senha = ''
-        self.__nivel_acesso = ''
 
     @property
-    def idparticipante(self):
-        return self.__idparticipante
+    def idusuario(self):
+        return self.__idusuario
 
     @property
     def nome(self):
         return self.__nome
-
-    @property
-    def prontuario(self):
-        return self.__prontuario
-
-    @property
-    def cpf(self):
-        return self.__cpf
 
     @property
     def email(self):
@@ -36,25 +22,13 @@ class Participante:
     def senha(self):
         return self.__senha
 
-    @property
-    def nivel_acesso(self):
-        return self.__nivel_acesso
-
-    @idparticipante.setter
-    def idparticipante(self, entrada):
-        self.__idparticipante = entrada
+    @idusuario.setter
+    def idusuario(self, entrada):
+        self.__idusuario = entrada
 
     @nome.setter
     def nome(self, entrada):
         self.__nome = entrada
-
-    @prontuario.setter
-    def prontuario(self, entrada):
-        self.__prontuario = entrada
-
-    @cpf.setter
-    def cpf(self, entrada):
-        self.__cpf = entrada
 
     @email.setter
     def email(self, entrada):
@@ -64,32 +38,22 @@ class Participante:
     def senha(self, entrada):
         self.__senha = entrada
 
-    @nivel_acesso.setter
-    def nivel_acesso(self, entrada):
-        self.__nivel_acesso = entrada
-
-    def setParticipante(self, idparticipante, nome, prontuario, cpf, email, senha, nivel_acesso):
-        self.__idparticipante = idparticipante
+    def setUsuario(self, idusuario, nome, email, senha):
+        self.__idusuario = idusuario
         self.__nome = nome
-        self.__prontuario = prontuario
-        self.__cpf = cpf
         self.__email = email
         self.__senha = senha
-        self.__nivel_acesso = nivel_acesso
 
     def inserirDados(self):
-        sql = ("insert into participante values ('{}','{}','{}','{}','{}','{}','{}')".format(
-            self.idparticipante,
+        sql = ("insert into usuario values ('{}','{}','{}','{}')".format(
+            self.idusuario,
             self.nome,
-            self.prontuario,
-            self.cpf,
             self.email,
             self.senha,
-            self.nivel_acesso,
         ))
         print(sql)
         return sql
 
     def buscar(self):
-        sql = "select count(*)+1 from participante"
+        sql = "select count(*)+1 from usuario"
         return sql
